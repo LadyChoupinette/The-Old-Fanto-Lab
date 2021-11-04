28e96daf73951980f9e532d0aa9594067471af19
GuillaumeJamin <guillaume.jamin@aptea.net>
2021-11-04 17:47:41 +0100

    fix web3 window

--- src/static/js/getWeb3.js
+++ src/static/js/getWeb3.js
@@ -1,5 +1,12 @@
 import Web3 from "web3";
 
-const web3 = new Web3(window.ethereum);
+let web3 = null;
+
+const isBrowser = typeof window !== "undefined"
+if (isBrowser) {
+    web3 = new Web3(window.ethereum);
+} else {
+    web3 = new Web3();
+}
 
 export default web3;
