c6580364d321c0ca2e804bc5e544fc2d0be17fd6
GuillaumeJamin <guillaume.jamin@aptea.net>
2021-11-06 10:12:36 +0100

    add cache prod

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,6 +7,7 @@ import {abi_Main, adrMain} from "../static/abi/abis";
 import {ButtonLeaveJourney} from "../components/Buttons/ButtonLeaveJourney";
 // import Web3 from 'web3'
 import {web3} from "../static/js/getWeb3";
+import Footer from "../components/Footer/Footer";
 
 function Index() {
 
@@ -15,11 +16,8 @@ function Index() {
     const [trainersIdle, setTrainersIdle] = useState(null);
     const [trainersHR, setTrainersHR] = useState(null);
     const [trainersJourney, setTrainersJourney] = useState(null);
-    const [walletAddress, setWallet] = useState("");
 
     let amount = 1
-
-
     useEffect(() => {
         const init = async () => {
 
@@ -66,39 +64,43 @@ function Index() {
 
     }, []);
 
-
     return (
         <main>
             <Helmet>
                 <title>The Old Fanto Lab</title>
             </Helmet>
-            <h1>The Old Fanto Lab</h1>v0.1.0
-            <p>
-                <React.Fragment>
-                    <span>Available trainer list :</span><br/>
-                        All trainers : {trainers ? trainers.toString() : 'loading trainers...'}<br/>
-                        Idle trainers : {trainersIdle ? trainersIdle.toString() : 'loading trainers...'}<br/>
-                        Healing trainers : {trainersHR ? trainersHR.toString() : 'loading trainers...'}<br/>
-                        Journey trainers : {trainersJourney ? trainersJourney.toString() : 'loading trainers...'}<br/>
-                </React.Fragment>
-                {/*<span>{trainers ? trainers.r : 'Loading Trainers...'}</span>*/}
-            </p>
-            <br/>
-            <p>
-                Click Enter Journey to send all your IDLE trainers on a Journey.
-                If they have been on a journey less than 12h ago, the transaction will fail and can be rejected when asked.
-                You can immediately leave by clicking the Leave button after the 'Enter' transaction succeeded.
+            <h1>The Old Fanto Lab</h1>
+            <p>Soon... The machines will awaken again, full of opportunities for those brave enough to venture in this remote wastlands.</p>
+            <br/><br/><br/>
 
-            </p>
-            <ButtonBatchJourney adr={adr} trainers={trainers} amount={amount}/>
-            <br/>
-            <br/>
+            <Footer/>
+
+        </main>
+    )}
+            {/*<p>*/}
+                {/*<React.Fragment>*/}
+                {/*    <span>Available trainer list :</span><br/>*/}
+                {/*        All trainers : {trainers ? trainers.toString() : 'loading trainers...'}<br/>*/}
+                {/*        Idle trainers : {trainersIdle ? trainersIdle.toString() : 'loading trainers...'}<br/>*/}
+                {/*        Healing trainers : {trainersHR ? trainersHR.toString() : 'loading trainers...'}<br/>*/}
+                {/*        Journey trainers : {trainersJourney ? trainersJourney.toString() : 'loading trainers...'}<br/>*/}
+                {/*</React.Fragment>*/}
+                {/*<span>{trainers ? trainers.r : 'Loading Trainers...'}</span>*/}
+            {/*</p>*/}
+            {/*<br/>*/}
+            {/*<p>*/}
+            {/*    Click Enter Journey to send all your IDLE trainers on a Journey.*/}
+            {/*    If they have been on a journey less than 12h ago, the transaction will fail and can be rejected when asked.*/}
+            {/*    You can immediately leave by clicking the Leave button after the 'Enter' transaction succeeded.*/}
+
+            {/*</p>*/}
+            {/*<ButtonBatchJourney adr={adr} trainers={trainers} amount={amount}/>*/}
+            {/*<br/>*/}
+            {/*<br/>*/}
             {/*<ButtonEnterJourney/>*/}
             {/*<ButtonLeaveJourney/>*/}
             {/*<h2>The date according to Node.js (TypeScript) is:</h2>*/}
             {/*<p>{date ? date : 'Loading date...'}</p>*/}
-        </main>
-    );
-}
+
 
 export default Index;
