3d26aba1c19460422ec09b30dfc4e3eed06b2ffc
GuillaumeJamin <guillaume.jamin@aptea.net>
2021-11-06 11:38:50 +0100

    add cache prod

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,8 @@
 import React, {useEffect, useState} from 'react';
 import {Helmet} from 'react-helmet';
 import '../styles/index.css';
-import {ButtonEnterJourney} from "../components/Buttons/ButtonEnterJourney"
 import {ButtonBatchJourney} from "../components/Buttons/ButtonBatchJourney"
 import {abi_Main, adrMain} from "../static/abi/abis";
-import {ButtonLeaveJourney} from "../components/Buttons/ButtonLeaveJourney";
 // import Web3 from 'web3'
 import {web3} from "../static/js/getWeb3";
 import * as loader from "../../.cache/commonjs/loader";
@@ -25,7 +23,8 @@ function Index() {
     const [trainersLegRelics, setTrainersLegRelics] = useState(null);
     const [trainersLegDouble, setTrainersLegDouble] = useState(null);
     const [trainersLegTriple, setTrainersLegTriple] = useState(null);
-    const [walletAddress, setWallet] = useState("");
+    const [errors, setErrors] = useState(null);
+    const [loadingState, setLoading] = useState("");
 
     let amount = 1
 
@@ -66,7 +65,6 @@ function Index() {
                 }
 
 
-
             }
 
             console.log(trainerList)
@@ -80,7 +78,7 @@ function Index() {
 
     }, []);
 
-    useEffect(()=> {
+    useEffect(() => {
         const initLeg = async () => {
             let trainersLeg = []
             let trainersLegRelics = []
@@ -91,64 +89,67 @@ function Index() {
             let trainersLegEntMaster = []
             let trainersLegDouble = []
             let trainersLegTriple = []
+            let errors = []
             await window.ethereum.enable()
             const contractMain = new web3.eth.Contract(abi_Main, adrMain);
             // console.log(await contractMain.options)
             const totalSupply = await contractMain.methods.totalSupply().call()
-                // .constantstalSupply().call()
+            // .constantstalSupply().call()
             for (let j = 0; j < totalSupply; j++) {
                 let rarity = "0";
-                let job="0";
-                let homeworld="0";
+                let job = "0";
+                let homeworld = "0";
                 try {
                     rarity = await contractMain.methods.getRarity(j).call()
-                job = await contractMain.methods.getClass(j).call()
-                homeworld = await contractMain.methods.getHomeworld(j).call()
-                } catch (e){
+                    job = await contractMain.methods.getClass(j).call()
+                    homeworld = await contractMain.methods.getHomeworld(j).call()
+                } catch (e) {
+                    errors.push(j)
                 }
-                let chill=0
-                if(rarity==='3'){
+                let chill = 0
+                if (rarity === '3') {
                     trainersLeg.push(j)
-                    chill=1
+                    chill = 1
                 }
                 switch (job) {
                     case '12':
-                        chill=chill+1
+                        chill = chill + 1
                         trainersLegEntMystic.push(j)
                         break;
                     case '13':
-                        chill=chill+1
+                        chill = chill + 1
                         trainersLegEntTheo.push(j)
                         break;
                     case '15':
-                        chill=chill+1
+                        chill = chill + 1
                         trainersLegEntMaster.push(j)
                         break;
                     case '14':
-                        chill=chill+1
+                        chill = chill + 1
                         trainersLegCosmic.push(j)
                         break;
                 }
-                switch (homeworld){
+                switch (homeworld) {
                     case "11":
-                        chill=chill+1
+                        chill = chill + 1
                         trainersLegAncient.push(j)
                         break;
                     case "12":
-                        chill=chill+1
+                        chill = chill + 1
                         trainersLegRelics.push(j)
                         break;
                 }
-                if(chill===2){
+                if (chill === 2) {
                     trainersLegDouble.push(j)
-                } else if (chill===3){
+                } else if (chill === 3) {
                     trainersLegTriple.push(j)
                 }
 
-                if(j%100===0){
-                    loader.BaseLoader=0
-                    loader.setLoader(parseInt((totalSupply/j).toString()))
-                    console.log("loading2 "+j)
+                if (j % 50 === 0) {
+                    loader.BaseLoader = 0
+                    loader.setLoader(parseInt((j / totalSupply * 100).toString()))
+                    setLoading(parseInt((j / totalSupply * 100).toString()))
+                    console.log("loading2 " + j)
 
                 }
             }
@@ -157,12 +158,14 @@ function Index() {
             setTrainersLegAncient(trainersLegAncient)
             setTrainersLegEntMystic(trainersLegEntMystic)
             setTrainersLegEntTheo(trainersLegEntTheo)
+            setTrainersLegEntMaster(trainersLegEntMaster)
             setTrainersLegCosmic(trainersLegCosmic)
             setTrainersLegTriple(trainersLegTriple)
             setTrainersLegDouble(trainersLegDouble)
+            setErrors(errors)
         }
         initLeg()
-    },[])
+    }, [])
 
 
     return (
@@ -174,17 +177,18 @@ function Index() {
             <p>
                 <React.Fragment>
                     <span>Available trainer list :</span><br/>
-                        All trainers : {trainers ? trainers.toString() : 'loading trainers...'}<br/>
-                        Idle trainers : {trainersIdle ? trainersIdle.toString() : 'loading trainers...'}<br/>
-                        Healing trainers : {trainersHR ? trainersHR.toString() : 'loading trainers...'}<br/>
-                        Journey trainers : {trainersJourney ? trainersJourney.toString() : 'loading trainers...'}<br/>
+                    All trainers : {trainers ? trainers.toString() : 'loading trainers...'}<br/>
+                    Idle trainers : {trainersIdle ? trainersIdle.toString() : 'loading trainers...'}<br/>
+                    Healing trainers : {trainersHR ? trainersHR.toString() : 'loading trainers...'}<br/>
+                    Journey trainers : {trainersJourney ? trainersJourney.toString() : 'loading trainers...'}<br/>
                 </React.Fragment>
                 {/*<span>{trainers ? trainers.r : 'Loading Trainers...'}</span>*/}
             </p>
             <br/>
             <p>
                 Click Enter Journey to send all your IDLE trainers on a Journey.
-                If they have been on a journey less than 12h ago, the transaction will fail and can be rejected when asked.
+                If they have been on a journey less than 12h ago, the transaction will fail and can be rejected when
+                asked.
                 You can immediately leave by clicking the Leave button after the 'Enter' transaction succeeded.
 
             </p>
@@ -192,15 +196,27 @@ function Index() {
             <br/>
             <br/>
             <p>
-            Leg Total trainers : {trainersLeg ? trainersLeg.length.toString()+" "+trainersLeg.toString() : 'loading trainers...'}<br/>
-            Trainers from Ancient Territories : {trainersLegAncient ? trainersLegAncient.length.toString()+" "+trainersLegAncient.toString() : 'loading trainers...'}<br/>
-            Trainers from Relics Rock : {trainersLegRelics ? trainersLegRelics.length.toString()+" "+trainersLegRelics.toString() : 'loading trainers...'}<br/>
-            Mystic Theorists : {trainersLegEntMystic ? trainersLegEntMystic.length.toString()+" "+trainersLegEntMystic.toString() : 'loading trainers...'}<br/>
-            Ent Theorists : {trainersLegEntTheo ? trainersLegEntTheo.length.toString()+" "+trainersLegEntTheo.toString() : 'loading trainers...'}<br/>
-            Cosmic Explorers : {trainersLegCosmic ? trainersLegCosmic.length.toString()+" "+trainersLegCosmic.toString() : 'loading trainers...'}<br/>
-            Ancient Ent Masters : {trainersLegAncient ? trainersLegAncient.length.toString()+" "+trainersLegAncient.toString() : 'loading trainers...'}<br/><br/>
-            Double Leg Trainers : {trainersLegDouble ? trainersLegDouble.length.toString()+" "+trainersLegDouble.toString() : 'loading trainers...'}<br/>
-            Triple Leg Trainers : {trainersLegTriple ? trainersLegTriple.length.toString()+" "+trainersLegTriple.toString() : 'loading trainers...'}<br/>
+                Leg Total trainers
+                : {trainersLeg ? trainersLeg.length.toString() + " " + trainersLeg.toString() : 'loading trainers...'}<br/>
+                Trainers from Ancient Territories
+                : {trainersLegAncient ? trainersLegAncient.length.toString() + " " + trainersLegAncient.toString() : 'loading trainers...'}<br/>
+                Trainers from Relics Rock
+                : {trainersLegRelics ? trainersLegRelics.length.toString() + " " + trainersLegRelics.toString() : 'loading trainers...'}<br/>
+                Mystic Theorists
+                : {trainersLegEntMystic ? trainersLegEntMystic.length.toString() + " " + trainersLegEntMystic.toString() : 'loading trainers...'}<br/>
+                Ent Theorists
+                : {trainersLegEntTheo ? trainersLegEntTheo.length.toString() + " " + trainersLegEntTheo.toString() : 'loading trainers...'}<br/>
+                Cosmic Explorers
+                : {trainersLegCosmic ? trainersLegCosmic.length.toString() + " " + trainersLegCosmic.toString() : 'loading trainers...'}<br/>
+                Ancient Ent Masters
+                : {trainersLegAncient ? trainersLegAncient.length.toString() + " " + trainersLegAncient.toString() : 'loading trainers...'}<br/><br/>
+                Double Leg Trainers
+                : {trainersLegDouble ? trainersLegDouble.length.toString() + " " + trainersLegDouble.toString() : 'loading trainers...'}<br/>
+                Triple Leg Trainers
+                : {trainersLegTriple ? trainersLegTriple.length.toString() + " " + trainersLegTriple.toString() : 'loading trainers...'}<br/>
+                <br/>
+                loading : {loadingState ? loadingState + "%" : "Récupération des datas en cours."} <br/>
+                {errors ? "Erreur de chargement des données : \n" + errors:""}
             </p>
             {/*<ButtonEnterJourney/>*/}
             {/*<ButtonLeaveJourney/>*/}
