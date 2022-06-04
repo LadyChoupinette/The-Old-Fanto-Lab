3210d63b25649ad861240b66c5fca71b2cfef9e5
LadyChoupinette <guillaumejamin42@gmail.com>
2022-06-04 15:47:36 +0200

    v3.2.0

---
+++ src/components/Buttons/MintPFPonies.jsx
@@ -0,0 +1,163 @@
+import React from 'react';
+import NumericInput from 'react-numeric-input';
+// import loadable from "@loadable/component";
+import ponyMint, { totalEggMints } from '../../static/js/PonyEggsUtils';
+// const ponyMint = loadable(() => import('../../static/js/PonyEggsUtils'));
+
+export default class PFPonies extends React.Component {
+  // totalMintsRes=0;
+  // amount = 1;
+
+  ponyMintParent;
+  getTotalMints;
+
+  constructor() {
+    super();
+    this.state = {
+      amount: 1,
+      totalMintsRes: 0,
+    };
+  }
+
+  myFormat(num) {
+    return num + ' PFPonies (25 $FTM each)';
+  }
+
+  async componentDidMount() {
+    this.ponyMintParent = async function (e) {
+      let f = await ponyMint(e);
+      console.log(f);
+    };
+    this.getTotalMints = async function () {
+      let res = await totalEggMints();
+      return res;
+    };
+    this.setState({
+      amount: this.state.amount,
+      totalMintsRes: await this.getTotalMints(),
+    });
+    // this.getTotalMints();
+    // this.forceUpdate();
+  }
+
+  //     useEffect(() =>
+  // {
+  //         const function init = async() => {
+  //             this.state.totalMintsRes = this.getTotalMints()
+  //     }
+  // })
+
+  amountChange(e) {
+    // this.forceUpdate();
+    console.log(e);
+    this.state = {
+      amount: e,
+    };
+  }
+
+  // useEffect(() => {
+  //
+  // })
+
+  render() {
+    return (
+      <div id="wrapperFields">
+        <br />
+        <label style={{ textAlign: 'center' }}>
+          <div>
+            <NumericInput
+              precision={0}
+              value={this.state.amount}
+              step={1}
+              format={this.myFormat}
+              onChange={(e) => this.amountChange(e)}
+              style={{
+                wrap: {
+                  background: '#000000 ',
+                  boxShadow: '0 0 1px 1px #ffffff inset, 1px 1px 5px -1px #000',
+                  padding: '2px 2.26ex 2px 2px',
+                  borderRadius: '6px 3px 3px 6px',
+                  fontSize: '32',
+                  width: '15em',
+                  marginLeft: '1em',
+                  marginRight: '1em',
+                },
+                input: {
+                  borderRadius: '4px 2px 2px 4px',
+                  color: '#ffffff',
+                  padding: '1ex 1ex !important',
+                  border: '1px solid #fff',
+                  marginRight: 4,
+                  display: 'block !important',
+                  fontWeight: 100,
+                  textShadow: '1px 1px 1px rgba(0, 0, 0, 0.1)',
+                },
+                'input:focus': {
+                  border: '1px inset #69C',
+                  outline: 'none',
+                },
+                arrowUp: {
+                  borderBottomColor: 'rgb(255,255,255)',
+                },
+                arrowDown: {
+                  borderTopColor: 'rgb(255,255,255)',
+                },
+              }}
+            />
+            <br />
+          </div>
+        </label>
+        <br />
+        <button
+          className="blackbg"
+          style={{
+            wrap: {
+              background: '#000000 ',
+              boxShadow: '0 0 1px 1px #ffffff inset, 1px 1px 5px -1px #000',
+              padding: '2px 2.26ex 2px 2px',
+              borderRadius: '6px 3px 3px 6px',
+              fontSize: '32',
+              width: '10em',
+              marginLeft: '1em',
+              marginRight: '1em',
+            },
+            input: {
+              borderRadius: '4px 2px 2px 4px',
+              color: '#ffffff',
+              padding: '1ex 1ex !important',
+              border: '1px solid #fff',
+              marginRight: 4,
+              display: 'block !important',
+              fontWeight: 100,
+              textShadow: '1px 1px 1px rgba(0, 0, 0, 0.1)',
+            },
+            'input:focus': {
+              border: '1px inset #69C',
+              outline: 'none',
+            },
+            arrowUp: {
+              borderBottomColor: 'rgb(255,255,255)',
+            },
+            arrowDown: {
+              borderTopColor: 'rgb(255,255,255)',
+            },
+          }}
+          onClick={() => this.ponyMintParent(this.state.amount)}
+        >
+          Mint !!!
+        </button>
+        <br />
+        <div>
+          <p
+            style={{
+              textShadow: ' 1px 1px 1px #ff8613',
+              fontSize: 'xxx-large',
+            }}
+          >
+            Total Mint : {this.state.totalMintsRes} / 1000 (at least...)
+          </p>
+        </div>
+      </div>
+    );
+  }
+}
--- src/pages/fields.js
+++ src/pages/fields.js
@@ -2,66 +2,80 @@ import React from 'react';
 // const Layout = loadable(() => import('../components/Layout'));
 import Layout from '../components/Layout';
 // import {PonyEggs} from "../components/Buttons/MintPonyEggs";
-import loadable from "@loadable/component";
-const PonyEggs = loadable(() => import('../components/Buttons/MintPonyEggs'));
+import loadable from '@loadable/component';
+// import PFPonies from '../components/Buttons/MintPFPonies';
+
+const PFPonies = loadable(() => import('../components/Buttons/MintPFPonies'));
 
 class Fields extends React.Component {
-    render() {
-        return (
-            <Layout fullMenu={false} page="fields">
-                <section id="banner-fields">
-                    <div className="inner-fields">
-                        <p style={{textShadow: ' 1px 1px 1px #00d93d'}}
-                        >
-                            You wander in the land around the The Old Lab. These lush grounds are filled with life,
-                            from little creatures in the herbs to those big eggs that definitly look like
-                            belongings to much bigger ones...
-                        </p>
-                            <br/>
-                            <br/>
-                        {/*<React.Fragment>*/}
-                            <PonyEggs/>
-                        {/*</React.Fragment>*/}
-                            <br/>
-                        <p style={{textShadow: ' 1px 1px 1px #d1ff00'}} >
-                            <span style={{
-                                position:'relative',
-                                fontSize: 'x-large',
-                            }}>(Each one is a free airdrop of PFPonies, very soon to come...)</span>
-                            <br/>
-                            OR<br/>
-                            <a href="https://paintswap.finance/marketplace/collections/0xa578d7f04ea7129f0ef81827846156438da65025"
-                               target="_blank">> Try to catch a little creature</a>
-                            {/*<p>*/}
-                            {/*    Send your fantomon trainers in a journey*/}
-                            {/*</p>*/}
-                            {/*<section id="one" className="wrapper spotlight">*/}
-                            {/*    <Journey/>*/}
-                            {/*</section>*/}
-                        </p>
-                    </div>
-                    <div id="arrow">
-                    </div>
-                </section>
-                <br/>
-                <br/>
-                <br/>
-                {/*<section id="wrapper-fields">*/}
-                {/*    <section id="one" className="wrapper-fields spotlight">*/}
-                {/*<Fields />*/}
-                {/*</section>*/}
+  render() {
+    return (
+      <Layout fullMenu={false} page="fields">
+        <section id="banner-fields">
+          <div className="inner-fields">
+            <h1>This is it !</h1>
+            <p className="pfponies">
+              <br />
+              <br />
+              The PFPonies are onto us ! God forbid us all !
+              <br />
+              <br />
+              You better catch as many of them as you can, who knows what
+              they're up to...
+            </p>
+            <br />
+            <br />
+            {/*<React.Fragment>*/}
+            <PFPonies />
+            {/*</React.Fragment>*/}
+            <br />
+            {/*<p style={{ textShadow: ' 1px 1px 1px #d1ff00' }}>*/}
+            {/*  <span*/}
+            {/*    style={{*/}
+            {/*      position: 'relative',*/}
+            {/*      fontSize: 'x-large',*/}
+            {/*    }}*/}
+            {/*  >*/}
+            {/*    (Each one is a free airdrop of PFPonies, very soon to come...)*/}
+            {/*  </span>*/}
+            {/*  <br />*/}
+            {/*  OR*/}
+            {/*  <br />*/}
+            {/*  <a*/}
+            {/*    href="https://paintswap.finance/marketplace/collections/0xa578d7f04ea7129f0ef81827846156438da65025"*/}
+            {/*    target="_blank"*/}
+            {/*  >*/}
+            {/*    > Try to catch a little creature*/}
+            {/*  </a>*/}
+            {/*  /!*<p>*!/*/}
+            {/*  /!*    Send your fantomon trainers in a journey*!/*/}
+            {/*  /!*</p>*!/*/}
+            {/*  /!*<section id="one" className="wrapper spotlight">*!/*/}
+            {/*  /!*    <Journey/>*!/*/}
+            {/*  /!*</section>*!/*/}
+            {/*</p>*/}
+          </div>
+          <div id="arrow"></div>
+        </section>
+        <br />
+        <br />
+        <br />
+        {/*<section id="wrapper-fields">*/}
+        {/*    <section id="one" className="wrapper-fields spotlight">*/}
+        {/*<Fields />*/}
+        {/*</section>*/}
 
-                {/*<section id="four" className="wrapper-fields alt style1">*/}
-                {/*    <div className="inner">*/}
-                {/*        <p className="major">*/}
-                {/*            The noises of the Old lab seems to attract strange creatures all around...*/}
-                {/*        </p>*/}
-                {/*    </div>*/}
-                {/*</section>*/}
-                {/*</section>*/}
-            </Layout>
-        );
-    }
+        {/*<section id="four" className="wrapper-fields alt style1">*/}
+        {/*    <div className="inner">*/}
+        {/*        <p className="major">*/}
+        {/*            The noises of the Old lab seems to attract strange creatures all around...*/}
+        {/*        </p>*/}
+        {/*    </div>*/}
+        {/*</section>*/}
+        {/*</section>*/}
+      </Layout>
+    );
+  }
 }
 
 export default Fields;
